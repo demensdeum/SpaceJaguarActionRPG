635d144dbaea3765af783a5e2fd8e12ed24da2d2
Ilia Prokhorov (Demensdeum) <demensdeum@gmail.com>
2020-04-26 11:34:31 +0300

    Remove temp directory after run

--- sabeplus-projects/linuxStatic/run.sh
+++ sabeplus-projects/linuxStatic/run.sh
@@ -3,6 +3,7 @@ mkdir build/linuxStatic/temp
 cp build/linuxStatic/FlameSteelEngineProject/FlameSteelEngineProject build/linuxStatic/temp
 rsync -avu --delete "project/resources/" "build/linuxStatic/FlameSteelEngineProject/"  
 cp build/linuxStatic/temp/FlameSteelEngineProject build/linuxStatic/FlameSteelEngineProject/FlameSteelEngineProject
+rm -rf build/linuxStatic/temp
 cd build/linuxStatic/FlameSteelEngineProject/
 ./FlameSteelEngineProject
 
