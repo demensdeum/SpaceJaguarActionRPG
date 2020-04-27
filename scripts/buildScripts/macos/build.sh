ecf32e1af244be4be60d3156c2984dbb8b34994f
Ilia Prokhorov (Demensdeum) <demensdeum@gmail.com>
2020-04-27 16:12:29 +0300

    Macos build scripts, resources

--- scripts/buildScripts/macos/build.sh
+++ scripts/buildScripts/macos/build.sh
@@ -1,1 +1,14 @@
- 
+source ~/Apps/osxcross.sh
+export OSXCROSS_MP_INC=1
+export OSXCROSS_NO_INCLUDE_PATH_WARNINGS=1
+buildDirectory=build/macos
+mkdir -p $buildDirectory || true
+cp scripts/buildScripts/macos/resources/CMakeLists.txt $buildDirectory/CMakeLists.txt
+FLAME_STEEL_PROJECT_ROOT_DIRECTORY=${PWD}
+FLAME_STEEL_PROJECT_ROOT_DIRECTORY_RESOURCES=${PWD}/project/resources/
+mkdir $buildDirectory/FlameSteelEngineProject || true
+cp project/resources/* $buildDirectory/FlameSteelEngineProject/
+cd $buildDirectory
+cmake -DFLAME_STEEL_PROJECT_ROOT_DIRECTORY=${FLAME_STEEL_PROJECT_ROOT_DIRECTORY} -DOSXCROSS=1 .
+make -j$(nproc)
+
--- scripts/buildScripts/windows/test.sh
+++ scripts/buildScripts/windows/test.sh
@@ -1,6 +1,5 @@
 rm -rf ~/.wine/drive_c/Games/SpaceJaguar || true
 mkdir ~/.wine/drive_c/Games/SpaceJaguar
-
 cp -r build/windows/FlameSteelEngineProject/* ~/.wine/drive_c/Games/SpaceJaguar
 cd ~/.wine/drive_c/Games/SpaceJaguar
 wine FlameSteelEngineProject.exe
