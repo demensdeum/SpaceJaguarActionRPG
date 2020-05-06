6b9410d64bf9daf66a48f9f577dc3ce8acaf7f96
Ilia Prokhorov (Demensdeum) <demensdeum@gmail.com>
2020-05-06 13:51:09 +0300

    Towns generator added

--- project/resources/com.demensdeum.spacejaguaractionrpg.scripts.sceneController.js
+++ project/resources/com.demensdeum.spacejaguaractionrpg.scripts.sceneController.js
@@ -2,7 +2,8 @@ function includeDependencies() {
     include("com.demensdeum.spacejaguaractionrpg.scripts.sceneController.constants.js");
     include("com.demensdeum.spacejaguaractionrpg.scripts.wallsgenerator.js");
     include("com.demensdeum.spacejaguaractionrpg.scripts.scenecontroller.enemies.js");
-    include("com.demensdeum.spacejaguaractionrpg.scripts.scenecontroller.jaguar.js");    
+    include("com.demensdeum.spacejaguaractionrpg.scripts.scenecontroller.jaguar.js");
+    include("com.demensdeum.spacejaguaractionrpg.scripts.towns.js");
 }
 
 function initializeIfNeeded() {
@@ -18,6 +19,7 @@ function initializeIfNeeded() {
     addJaguarToScene();
     addEnemiesToScene();
     addWallsToScene();
+    addTownsToScene();
 };
 
 function step() {
@@ -27,4 +29,4 @@ function step() {
 };
 
 step();
-//GRANNYPILLS();
+GRANNYPILLS();
